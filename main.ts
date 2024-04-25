913264e8968a15f5ce88cc63d9ea8d0a90e9a629
AlizyKhi <162714208+AlizyKhi@users.noreply.github.com>
2024-04-25 08:59:26 +0500

    Add files via upload
    Do Practice With Me

---
+++ main.ts
@@ -0,0 +1,22 @@
+let profileIcon:string = "Triple,Cases"
+let profile_icon:string = "Triple,Cases"
+let ProfileIcon:string = "Triple,Cases"
+
+
+let isProfileIcon:number = 1000
+let is_profile_icon:number = 900
+let IsProfileIcon:number = 800
+
+
+let thatIsProfileIcon:boolean = true;
+let that_is_profile_icon:boolean = false;
+let ThatIsProfileIcon:boolean = true;
+
+
+console.log(profileIcon,profile_icon,ProfileIcon)
+console.log(isProfileIcon,is_profile_icon,IsProfileIcon)
+console.log(thatIsProfileIcon,that_is_profile_icon,ThatIsProfileIcon)
+
+
+
+
